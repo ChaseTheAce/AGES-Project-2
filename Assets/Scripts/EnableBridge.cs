39e6e630b4d5d1dd2d750a96be8a6c041dc36185
ChaseTheAce <32078056+ChaseTheAce@users.noreply.github.com>
2018-05-06 23:59:24 -0500

    Bridge

---
+++ Assets/Scripts/EnableBridge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableBridge : MonoBehaviour {
+
+    [SerializeField]
+    GameObject bridge;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bridge.SetActive(false);
+    }
+}
